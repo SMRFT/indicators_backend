976ef7ce38ef95e63b511e8bb175a525300212b4
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-05-14 13:15:46 +0530

    Update permissions_map.py

---
+++ forms/auth/permissions_map.py
@@ -0,0 +1,16 @@
+PAGE_MAPPING = {
+  
+}
+
+PAGE_ACTION_MAPPING = {
+    'xxx': {
+        'DELETE':'RWD',
+    },
+}
+
+GEN_ACTION_MAPPING = {
+    'POST': 'RW',
+    'PUT': 'RW',
+    'DELETE': 'RW',
+    'GET': 'R',
+}
