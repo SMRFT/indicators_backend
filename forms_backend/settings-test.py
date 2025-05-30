eea9c25e79b77a80375a966fc8a39692cfeba4a1
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-05-30 15:00:29 +0530

    Update settings-test.py

--- forms_backend/settings-test.py
+++ forms_backend/settings-test.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'env-5683222.in1.cloudlets.co.in']
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    'http://localhost:3000','https://test.shinova.in'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
